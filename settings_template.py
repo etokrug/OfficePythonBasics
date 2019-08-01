ed49dfe16bdbf8af154ccdea6ffd034c445614ca
etokrug <etokrug@gmail.com>
2019-08-01 09:33:03 -0400

    cleanup

---
+++ settings_template.py
@@ -0,0 +1,16 @@
+# Rename this file to settings.py
+
+# Your UPS username
+USERNAME = ''
+
+# Your UPS password
+PASS = ''
+
+# A Census API key you can request here: https://api.census.gov/data/key_signup.html
+CENSUS_KEY = ''
+
+# Add your company's proxy URL here
+PROXY = ''
+
+# The variable we'll use to send the message via a company proxy (otherwise it won't work)
+PROXIES = { 'https' : 'https://{user}:{password}@{proxy_url}:8080'.format(user=USERNAME, password=PASS, proxy_url=PROXY) } 
